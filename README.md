e8ec8f8b9b4864c137cc94b78a2336967b7bf53c
Replit user <>
2023-10-17 11:44:44 +0000

    completed

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # WebScrapperByFlask
 first time to use Flask
+
+This is the code that search for a keyword input by user at sites getwork/we work remotely and show the results in browser
---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, redirect, send_file
+from extractors.getwork import extract_getwork
+from extractors.wwr import extract_wwr_jobs
+from file import save_to_file
+
+app = Flask("JobScrapper")
+
+db = {}
+
+
+@app.route("/")
+def home():
+  return render_template("home.html", name="cham")
+
+
+@app.route("/search")
+def search():
+  keyword = request.args.get("keyword")
+  if keyword == None:
+    return redirect("/")
+  if keyword in db:
+    jobs = db[keyword]
+  else:
+    getwork = extract_getwork(keyword)
+    wwr = extract_wwr_jobs(keyword)
+    jobs = getwork + wwr
+    db[keyword] = jobs
+
+  return render_template("search.html", keyword=keyword, jobs=jobs)
+
+
+@app.route("/export")
+def export():
+  keyword = request.args.get("keyword")
+  if keyword == None:
+    return redirect("/")
+  if keyword not in db:
+    return redirect(f"/search?keyword={keyword}")
+  save_to_file(keyword, db[keyword])
+  return send_file(f"{keyword}.csv", as_attachment=True)
+
+
+app.run("0.0.0.0")
